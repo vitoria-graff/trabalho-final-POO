1504159ff1134a4a162650eb86691d3d9a2bb81b
vitoria.graff <vitoria.graff@edu.pucrs.br>
2024-06-28 18:47:31 -0300

    att

---
+++ src/dados/Agricola.java
@@ -0,0 +1,26 @@
+package dados;
+
+public class Agricola extends Robo {
+
+	private double area;
+	private String uso;
+
+	public Agricola(int id, String modelo, double valorDiario, double area, String uso) {
+		super(id, modelo, valorDiario);
+		this.area=area;
+		this.uso=uso;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public String getUso() {
+		return uso;
+	}
+
+	@Override
+	public double calculaLocacao(int dias) {
+		return 0;
+	}
+}
